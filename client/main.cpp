31adf2e8c22fc2e55fd6e1c84f6b2dd7019f8a96
s7k-windows <benotimprotant@mail.ru>
2026-03-11 21:41:58 +0300

    minimal client-server communication

--- client/main.cpp
+++ client/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <filesystem>
 
 #include <charconv>
 
 #include "tcp/tcp.h"
+
+#include "thirdparty/nlohmann/json.hpp"
+using json = nlohmann::json;
+
+#define PROTOCOL_VERSION 1
 
 int main(int argc, char** argv) {
     if (argc < 3) {
@@ -28,12 +34,72 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    TCPServer server("0.0.0.0", 443);
+    std::ifstream file(filepath, std::ios::binary);
+    size_t filesize = std::filesystem::file_size(filepath);
+    
+    TCPClient conn;
+    conn.connect("127.0.0.1", port);
 
-    server.listen(10);
-    server.accept();
+    std::string request = R"(
+        {
+            "protocol": {
+                "version": )" + std::to_string(PROTOCOL_VERSION) + R"(
+            },
+            "request": "check",
+            "filesize": )" + std::to_string(filesize) + R"(
+        }  
+    )";
 
-    std::cout << "Accepted" << std::endl;
+    conn.send(request);
+
+    std::string replyStr = conn.receive();
+    // std::cout << replyStr << std::endl;
+    json reply = json::parse(replyStr);
+
+    std::string reply_status = reply["status"].get<std::string>();
+
+    if (reply_status == "reject") {
+        std::cout << "Request was rejected by server" << std::endl;
+        std::string reason = reply["reason"].get<std::string>();
+        std::cout << "Reason: " << reason << std::endl;
+        return 0;
+    } else if (reply_status != "accept") {
+        std::cerr << "Protocol violation from server: expected `reject` or `accept`, got `" + reply_status + "`" << std::endl;
+        return 1;
+    }
+
+    size_t sentSize = 0;
+    size_t chunkSize = 1024;
+    std::string buffer(chunkSize, '\0');
+    while (sentSize < filesize) {
+        file.read(&buffer[0], chunkSize);
+        size_t bytesRead = file.gcount();
+        if (bytesRead == chunkSize) {
+            conn.send(buffer);
+        } else {
+            std::string chunk = buffer.substr(0, bytesRead);
+            conn.send(chunk);
+        }
+        sentSize += bytesRead;
+        // std::cout << sentSize << "/" << filesize << std::endl;
+    }
+
+    std::cout << "File sent, waiting for results..." << std::endl;
+
+    std::string resultStr = conn.receive(4096);
+    // std::cout << resultStr << std::endl;
+    json result = json::parse(resultStr);
+
+    bool dangerous = result["dangerous"].get<bool>();
+    if (dangerous) {
+        std::cout << "File is dangerous" << std::endl;
+    } else {
+        std::cout << "File is safe" << std::endl;
+    }
+    
+    if (result.contains("description")) {
+        std::cout << "Description: " << result["description"] << std::endl;
+    }
 
     return 0;
 }
---
+++ server/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+
+#include <charconv>
+
+#include "tcp/tcp.h"
+
+#include "thirdparty/nlohmann/json.hpp"
+using json = nlohmann::json;
+
+#define PROTOCOL_VERSION 1
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cerr << "Too few arguments" << std::endl;
+        std::cerr << "Usage:" << std::endl;
+        std::cerr << "\tclient [path] [port]" << std::endl;
+        return 1;
+    }
+
+    std::string filepath = argv[1];
+    std::string portstring = argv[2];
+
+    int port;
+    auto [ptr, ec] = std::from_chars(portstring.data(), portstring.data() + portstring.size(), port);
+
+    if (ec == std::errc::invalid_argument) {
+        std::cerr << "Port must be a number" << std::endl;
+        return 1;
+    } else if (ec == std::errc::result_out_of_range) {
+        std::cerr << "Port number is too big" << std::endl;
+        return 1;
+    }
+
+    TCPServer conn("0.0.0.0", port);
+    conn.listen(10);
+
+    while (true) {
+        try {
+            auto clientConn = conn.accept();
+
+            std::string requestStr = clientConn->receive();
+            std::cout << requestStr << std::endl;
+            json request = json::parse(requestStr);
+            
+            json protocol = request["protocol"];
+            int version = protocol["version"].get<int>();
+
+            size_t filesize = request["filesize"].get<size_t>();
+
+            if (PROTOCOL_VERSION != version) {
+                std::string reply = R"(
+                {
+                    "status" : "reject",
+                    "reason" : "Different versions. )" + std::to_string(PROTOCOL_VERSION) + R"( (server) vs )" + std::to_string(version) + R"( (client) "
+                }
+                )";
+                clientConn->send(reply);
+            }
+
+            std::string reply = R"(
+                {
+                    "status" : "accept"
+                }
+            )";
+            clientConn->send(reply);
+
+            size_t readSize = 0;
+            size_t chunkSize = 1024;
+            std::string buffer;
+            while (readSize < filesize) {
+                std::string chunk = clientConn->receive();
+                readSize += chunk.size();
+                std::cout << readSize << "/" << filesize << std::endl;
+            }
+
+            std::string result = R"(
+                {
+                    "dangerous" : false,
+                    "description" : "No real check, just testing"
+                }
+            )";
+            clientConn->send(result);
+        } catch (std::exception e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+    
+    return 0;
+}
